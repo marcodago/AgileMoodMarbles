5d9deb17bb7c286f5e4dfd23af60649dc078c802
marcodago <marcodago@it.ibm.com>
2017-11-02 22:35:10 +0100

    Initial version

--- Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
+++ Pods/Charts/Source/Charts/Charts/BarLineChartViewBase.swift
@@ -1545,7 +1545,7 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
     }
     
     /// is dragging on the X axis enabled?
-    open var dragXEnabled: Bool 
+    @objc open var dragXEnabled: Bool 
     {
         get
         {
@@ -1558,7 +1558,7 @@ open class BarLineChartViewBase: ChartViewBase, BarLineScatterCandleBubbleChartD
     }
     
     /// is dragging on the Y axis enabled?
-    open var dragYEnabled: Bool
+    @objc open var dragYEnabled: Bool
     {
         get
         {
